c16301a94f2c06d793aecf2c1958670af6640612
Nndaaa404 <anandaarizkymaulana@gmail.com>
2023-12-25 09:38:40 +0700

    perbaikan 4

--- js/spreadsheets.js
+++ js/spreadsheets.js
@@ -1,28 +1,28 @@
 const scriptURL = "https://script.google.com/macros/s/AKfycbzBOAKCwjsFCwov9XyCqY2wDkxwu6utgDsUhQuA_OjGTOpseWVqmxRIO-CiZjyRVvOg/exec";
 const form = document.forms["portfolio-contact-form"];
-        const alertBox = document.getElementById("myAlert");
-        const alertMessage = document.getElementById("alertMessage");
+const alertBox = document.getElementById("myAlert");
+const alertMessage = document.getElementById("alertMessage");
 
-        form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            fetch(scriptURL, { method: "POST", body: new FormData(form) })
-                .then((response) => {
-                    form.reset();
-                    displayAlert("Success! Form submitted successfully.", "success");
-                    console.log("Success!", response);
-                })
-                .catch((error) => {
-                    displayAlert("Error submitting form. Please try again.", "error");
-                    console.error("Error!", error.message);
-                });
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => {
+            form.reset();
+            displayAlert("Success! Form submitted successfully.", "success");
+            console.log("Success!", response);
+        })
+        .catch((error) => {
+            displayAlert("Error submitting form. Please try again.", "error");
+            console.error("Error!", error.message);
         });
+});
 
-        function displayAlert(message, type) {
-            alertMessage.textContent = message;
-            alertBox.style.display = "block";
-            alertBox.className = `alert ${type}`;
-        }
+function displayAlert(message, type) {
+    alertMessage.textContent = message;
+    alertBox.style.display = "block";
+    alertBox.className = `alert ${type}`;
+}
 
-        function closeAlert() {
-            alertBox.style.display = "none";
-        }
+function closeAlert() {
+    alertBox.style.display = "none";
+}
